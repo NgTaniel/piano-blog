69f8fdca3986ba86de95e907b33cf9465c8f39c0
Nathaniel <nathanieltan@LAPTOP-KPDE7V63>
2023-11-11 20:19:26 +1100

    work in progress

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/NavBar';
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,37 @@
+export const Menu = [
+  {
+    title: 'Home',
+    url: './',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Mini Keyboard',
+    url: './mini-keyboard',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Keyboard',
+    url: '/keyboard',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Upright',
+    url: '/upright',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Grand',
+    url: '/grand',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Extended Grand',
+    url: '/extended-grand',
+    cName: 'nav-links'
+  },
+  {
+    title: 'aww &#128557',
+    url: '/aww',
+    cName: 'nav-links'
+  },
+]
